3d37d7c979d7ce751d1afcdb3d7e3b5157d853c6
Piumi-Anuradha <piumianuradha26@gmail.com>
2024-04-15 12:54:34 +0000

    update models.py

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ from taskmanager import db
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    task = db.relationship("Recipe", backref="category", cascade="all,delete",lazy=True)
+    tasks = db.relationship("Recipe", backref="category", cascade="all,delete",lazy=True)
 
     def __repr__(self):
         return self.category_name
